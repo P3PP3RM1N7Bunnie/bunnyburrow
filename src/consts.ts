46bb015b26b141ae3018971ebfd27b6c9e46cdf2
ryva <133610220+P3PP3RM1N7Bunnie@users.noreply.github.com>
2025-11-24 06:30:09 +0000

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "𝑩𝒖𝒏𝒏𝒚 𝑩𝒖𝒓𝒓𝒐𝒘";
+export const SITE_DESCRIPTION = "Welcome to my teeny corner of the internet!";
